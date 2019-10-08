63b4dfec8c6a67b03ab365ca2df523bb23ae4487
motomohan <56292208+motomohan@users.noreply.github.com>
2019-10-08 12:49:33 +0530

    sample get service
    sample get service

---
+++ sample/src/main/java/com/cloud/sample/controller/SampleRestController.java
@@ -0,0 +1,14 @@
+package com.cloud.sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleRestController {
+
+	
+	@GetMapping(value="/getService")
+	public String getSampleService() {
+		return "Hello.. This is a sample rest service response";
+	}
+}
